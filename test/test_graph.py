7854a1bdaf258206ee8588b724b0dd52a0534291
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-08 13:27:59 +0200

    Update test_graph.py

--- test/test_graph.py
+++ test/test_graph.py
@@ -1,9 +1,12 @@
 
 from src.graph import generate_edges
 
-def test_generate_edges():
+def test_generate_edges() -> None:
+    """Small test for the number of edges and edge-duplication"""
     n = 5
-    edges = set(generate_edges(n))
+    edges = list(generate_edges(n))
+    unique_edges = set(edges)
+    expected_edge_count = 2 * n * (n - 1)
 
-    assert len(edges) == 2 * n * (n - 1)
-    assert len(edges) == len(set(edges)) 
+    assert len(edges) == expected_edge_count, f"Expected number of edges: {expected_edge_count}, actual: {len(edges)}"
+    assert len(edges) == len(unique_edges), "There are duplicate generated edges."
