5caf02c2b6444272e493570c8980a3e4fbc0ec47
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-08 13:24:11 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,28 +3,25 @@ from src.graph import add_parameters
 from src.percolation import largest_component_evolution
 from src.plotting import plot_func
 
-def main():
-
-    parser = argparse.ArgumentParser()
+def main() -> None:
+    """Interface for the percolation simulation"""
+    parser = argparse.ArgumentParser(description="2D grid percolation simulation")
 
     parser.add_argument(
         "--n",
         type=int,
         default=100,
-        help="Grid side length"
+        help="size of the side of the grid (initially: 100)"
     )
 
     args = parser.parse_args()
-
     n = args.n
 
-    print(f"Running simulation with n={n}")
-
-    edges = list(add_parameters(n))
-
-    result = largest_component_evolution(n, edges)
+    print(f"Starting simulation with n={n}... (Grid size: {n}x{n})")
+    
 
     plot_func(n)
+    print("Simulation terminated succesfully.")
 
 if __name__ == "__main__":
     main()
