87309fdddb68fdea1184ea73651c3b5a63dd2407
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-08 13:37:23 +0200

    Update test_graph.py

--- test/test_graph.py
+++ test/test_graph.py
@@ -1,12 +1,16 @@
-
 from src.graph import generate_edges
 
-def test_generate_edges() -> None:
-    """Small test for the number of edges and edge-duplication"""
-    n = 5
+def test_generate_edges(grid_size: int) -> None:
+    """
+    Testing the number of edges if the gridsize is given, and testing for duplicate edges.
+    """
+    n = grid_size
+    print(f"\nruninng test with grid side size n={n}...") 
+
     edges = list(generate_edges(n))
     unique_edges = set(edges)
+
     expected_edge_count = 2 * n * (n - 1)
 
     assert len(edges) == expected_edge_count, f"Expected number of edges: {expected_edge_count}, actual: {len(edges)}"
-    assert len(edges) == len(unique_edges), "There are duplicate generated edges."
+    assert len(edges) == len(unique_edges), "There is duplication between the generated edges."
