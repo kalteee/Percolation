17baa882c1f090f50b98c9d4f46bf5e0465ce489
B. Bence Kaltenecker <kaltenecker.bence04@gmail.com>
2026-06-08 13:09:47 +0200

    Update unionfind.py

--- src/unionfind.py
+++ src/unionfind.py
@@ -1,15 +1,36 @@
-class UnionFind: #for the size of the maximal cardinality component
-    def __init__(self, n):
+class UnionFind: 
+    """
+    Union-Find data structure for the update of connected components and maximal size component on the grid.
+    """
+    def __init__(self, n: int) -> None:
+        """Initialization of the data structure with n elements (vertices)"""
         self.roots = [i for i in range(n)]
         self.sizes = [1] * n
         self.maxsize = 1 if n > 0 else 0
         
-    def find(self, a):
+    def find(self, a: int) -> int:
+        """
+        Find the representative (root) of the element a with path-compression.
+        
+        Args: a (int): the index of the element in search
+        Returns: 
+            int: the root of the component of the vertex
+        """
         if a != self.roots[a]:
             self.roots[a] = self.find(self.roots[a])
         return self.roots[a]
 
-    def union(self, a, b):
+    def union(self, a: int, b: int) -> "UnionFind":
+
+        """
+        Merge the components containing 'a' and 'b' vertices. Update the size of the maximal sized component.
+        
+        Args:
+            a (int): index of the first vertex.
+            b (int): index of the second vertex.
+        Returns:
+            UnionFind: returns self for linking aspects.
+        """
         ra, rb = self.find(a), self.find(b)
         if ra != rb:
             if self.sizes[ra] < self.sizes[rb]:
@@ -24,6 +45,7 @@ class UnionFind: #for the size of the maximal cardinality component
                     self.maxsize = self.sizes[ra]
         return self
     
-    def get_maxsize(self):
+    def get_maxsize(self) -> int:
+        """Returns the size of the maximal cardinality connected component"""
         return self.maxsize
     
